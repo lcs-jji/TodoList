8523a577327a603be778ac11325280c6855df796
Joseph Ji <jji@lcs.on.ca>
2025-01-28 12:18:45 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Xinchen Ji on 2025-01-28.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
