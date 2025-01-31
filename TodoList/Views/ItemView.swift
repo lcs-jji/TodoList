4e88b62386859dd0767cedd136db303429dd7b27
Joseph Ji <jji@lcs.on.ca>
2025-01-30 21:22:49 -0500

    Got Xcode Previews working for the ItemView structure (which uses the @Bindable property wrapper, and so requires a slightly different approach).

--- TodoList/Views/ItemView.swift
+++ TodoList/Views/ItemView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ItemView: View {
     
@@ -26,9 +27,14 @@ struct ItemView: View {
     }
 }
 
-//#Preview {
-//    List {
-//        ItemView(currentItem: Binding.constant(firstItem))
-//        ItemView(currentItem: Binding.constant(secondItem))
-//    }
-//}
+#Preview {
+        
+        let container = TodoItem.preview
+        
+        return List {
+            ItemView(currentItem: TodoItem.someItem)
+            ItemView(currentItem: TodoItem.anotherItem)
+        }
+        .modelContainer(container)
+        
+}
