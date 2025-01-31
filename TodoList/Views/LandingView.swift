1db1bf3433d19d20c7b60b5af4b780c975f43e13
Joseph Ji <jji@lcs.on.ca>
2025-01-30 20:34:25 -0500

    Made it possible to add a new to-do item.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -26,14 +26,26 @@ struct LandingView: View {
                     
                     TextField("Enter a to-do item", text: $newItemDescription)
                     Button("ADD") {
-                        
+                        createToDo(withTitle: newItemDescription)
                     }
                     .font(.caption)
+                    .disabled(newItemDescription.isEmpty == true)
                 }
                 .padding(20)
             }
             .navigationTitle("To Do")
         }
+    }
+    
+    func createToDo(withTitle title: String) {
+        
+        let todo = TodoItem(
+            title: title,
+            done: false
+        )
+        
+        todos.append(todo)
+        
     }
 }
 
